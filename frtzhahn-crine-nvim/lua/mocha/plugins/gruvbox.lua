a48dca22b507e33fbdd6b85777e368c074e5df2e
aikhe <ikeandrie.ro@gmail.com>
2025-12-04 10:57:31 +0800

    chore: remove frtzhahn-crine-nvim directory

--- frtzhahn-crine-nvim/lua/mocha/plugins/gruvbox.lua
+++
@@ -1,8 +0,0 @@
-return {
- "ellisonleao/gruvbox.nvim", priority = 1000 ,
-      config = function()
-        vim.o.background = "dark" -- Or "light"
-        vim.cmd.colorscheme("gruvbox")
-      end,
- opts = ...
-}
